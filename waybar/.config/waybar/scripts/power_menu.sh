22d7cc1a5d19d65e919d28bc147ed4a2cd360d45
saurav <sauravbajya@gmail.com>
2026-01-03 14:34:54 +0545

    modify waybar's power_menu.sh for wofi

--- waybar/.config/waybar/scripts/power_menu.sh
+++ waybar/.config/waybar/scripts/power_menu.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Display a menu to choose action
-action=$(echo -e "Shutdown\nReboot\nSuspend\nHibernate" | rofi -dmenu -prompt "Power Menu:")
+# Options with icons
+entries=" Shutdown\n Restart\n Suspend\n Hibernate\n Logout\n Lock"
 
-case "$action" in
-Shutdown)
-  systemctl poweroff
-  ;;
-Reboot)
-  systemctl reboot
-  ;;
-Suspend)
-  systemctl suspend
-  ;;
-Hibernate)
-  systemctl hibernate
-  ;;
+selected=$(echo -e "$entries" | wofi --width 250 --height 380 --dmenu --cache-file /dev/null --prompt "Power Menu" --allow-images=false | awk '{print tolower($2)}')
+
+case "$selected" in
+  shutdown)
+    systemctl poweroff ;;
+  restart)
+    systemctl reboot ;;
+  suspend)
+    systemctl suspend ;;
+  hibernate)
+    systemctl hibernate ;;
+  logout)
+    hyprctl dispatch exit ;;
+  lock)
+    hyprlock ;;  # Or swaylock if you use that
 esac
